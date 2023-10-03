03c18ad9229c089fc25524d23ba210fd984173ad
Meet Sardhara <meet.scalelot@gmail.com>
2023-10-03 16:14:34 +0530

    delete all files

--- utilities/connectDB.js
+++
@@ -1,6 +0,0 @@
-let mongoose = require('mongoose');
-let mongoDB = mongoose.createConnection(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-module.exports = mongoDB;
--- utilities/constants.js
+++
@@ -1,7 +0,0 @@
-let constants = {
-    DEFAULT_DB: process.env.DATABASE,
-    MODELS: {
-        users: 'Users'
-    }
-};
-module.exports = constants;
